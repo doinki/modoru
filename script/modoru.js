582288f2a98bcbd3143842f9ff36871c95a51796
doinki <90969158+doinki@users.noreply.github.com>
2024-10-22 18:32:58 +0900

    🐛 (#2)

--- script/modoru.js
+++ script/modoru.js
@@ -4,16 +4,12 @@
   let originalPushState = window.history.pushState.bind(window.history);
   let key = 'MODORU_INDEX';
 
-  window.history.pushState = new Proxy(window.history.pushState, {
-    apply(_, __, [data, unused, url]) {
-      return originalPushState(
-        {
-          [key]: (window.history.state[key] || 0) + 1,
-          ...data,
-        },
-        unused,
-        url,
-      );
+  Object.defineProperty(window.history, 'pushState', {
+    configurable: true,
+    value(data, unused, url) {
+      data[key] = ((window.history.state || {})[key] || 0) + 1;
+
+      return originalPushState(data, unused, url);
     },
   });
 })();
